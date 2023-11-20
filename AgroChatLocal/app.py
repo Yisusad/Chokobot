70b8fb1dd7ffcc20155e86e266ed4cebb8dbc012
Jesús Acuña <71677639+Yisusad@users.noreply.github.com>
2023-11-20 08:19:16 -0300

    Add files via upload

--- AgroChatLocal/app.py
+++ AgroChatLocal/app.py
@@ -1,8 +1,8 @@
 import streamlit as st
 import openai
+__import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
-import sqlite3
 from dotenv import load_dotenv
 from langchain.document_loaders import PyPDFDirectoryLoader
 from langchain.text_splitter import CharacterTextSplitter
